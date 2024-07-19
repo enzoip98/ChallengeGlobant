d0e3b6ebd15a671aaed40a96e7f4d8f48e5f51fa
enzo ipanaque <eipanaquep@pucp.edu.pe>
2024-07-18 21:30:38 -0500

    fix

--- src/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URI
-
-engine = create_engine(DATABASE_URI)
-Session = sessionmaker(bind=engine)
-session = Session()
